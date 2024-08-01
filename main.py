71bd4679383e272140fc5e1616a3263e74e7a05c
Sergi0iv <rickitir2.0@gmail.com>
2024-08-01 20:51:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# '1st program'
+print(9**0.5*5)
+# '2hd program'
+print(9.99>9.98,1000!=1000.1)
+# '3rd program'
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+# '4th program'
+num1 = int(42.13)
+num2 = 13.42
+print(num1==int(round(num2%1,2)*100))
